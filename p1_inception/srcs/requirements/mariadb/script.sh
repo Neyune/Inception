7737084da7effe5891c349e9486a984a819a5f40
lara <ereali@student.42.fr>
2023-01-25 14:30:18 +0100

    mariadb inprogress

--- p1_inception/srcs/requirements/mariadb/script.sh
+++ p1_inception/srcs/requirements/mariadb/script.sh
@@ -2,6 +2,15 @@
 touch /tmp_file
 chmod 700 /tmp_file
 
+systemctl service mysql start
+
+cat << EOF > /tmp_file
+CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+GRANT ALL ON wordpress.* TO 'wordpress_user'@'localhost' IDENTIFIED BY 'password';
+FLUSH PRIVILEGES;
+EOF
+mariadb -u root < /tmp_file
 # creer un fichier temporaire dans lequel on met les instruction mysql 
 # les rediriger dans mysql
-exec /usr/bin/mysqld --user=mysql --console
+
+exec /usr/bin/mysqld --user=mysql 

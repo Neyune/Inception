3f1b2e1c6ce1ae3bc6d60c7c8b5e89c379d6e178
System Administrator <realielsa@gmail.com>
2023-01-26 17:55:44 +0100

    wordpess working

--- p1_inception/srcs/requirements/wordpress/wp-script.sh
+++ p1_inception/srcs/requirements/wordpress/wp-script.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-if ! [ -f "/var/www/wordpress/wp-config.php" ]; then
-    wp config create --dbname='db_name' --dbuser='user_name' --dbpass='user_pass' --dbhost='mysql' --dbprefix='wp_' --allow-root 
-wp core install  --path='/var/www/html' --url='http://ereali.42.fr' --title='Blog Title' --admin_user='adminuser' --admin_password='password' --admin_email='ereali@student.42.fr' --allow-root
-touch /var/log/access.log
-touch /var/log/error.log
+# tail -f /dev/null
+if ! [ -f "/var/www/html/wp-config.php" ]; then
+	mv /root/html /var/www/
+    wp config create --path='/var/www/html' --dbname='db_name' --dbuser='user_name' --dbpass='user_pass' --dbhost='mysql' --dbprefix='wp_' --allow-root
+	wp core install  --path='/var/www/html' --url='http://ereali.42.fr' --title='Blog Title' --admin_user='adminuser' --admin_password='password' --admin_email='ereali@student.42.fr' --allow-root
+	touch /var/log/access.log
+	touch /var/log/error.log
 fi
-exec php-fpm7.3
+# tail -f /dev/null
+
+exec php-fpm7.3 -FR

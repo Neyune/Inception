09efd2749e105b2306a6f673ec93dac7f136ccaa
lara <ereali@student.42.fr>
2023-01-27 21:41:05 +0100

    allllllerrr

--- p1_inception/srcs/requirements/mariadb/script.sh
+++ p1_inception/srcs/requirements/mariadb/script.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
-touch /tmp_file
-chmod 755 /tmp_file
+
+# tail -f /dev/null
+# MYSQL_DATABASE=inception
+# MYSQL_ROOT_PASSWORD=wordpress
+# MYSQL_ROOT_USER=root
+# MYSQL_USER=emaugale
+# MYSQL_PASSWORD=1234
+
+touch /tmp/tmp_file
+chmod 755 /tmp/tmp_file
 
 service mysql start
 
 cat << EOF > /tmp_file
-CREATE DATABASE db_name DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE DATABASE IF NOT EXISTS db_name DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 GRANT ALL ON *.* TO 'user_name'@'localhost' IDENTIFIED BY 'user_pass';
 FLUSH PRIVILEGES;
 EOF
-mysql < /tmp_file
-# creer un fichier temporaire dans lequel on met les instruction mysql 
-# les rediriger dans mysql
+mysql < /tmp/tmp_file
+# # creer un fichier temporaire dans lequel on met les instruction mysql 
+# # les rediriger dans mysql
 service mysql stop
+rm -rf /tmp/tmp_file
 
 exec mysqld --user=mysql --bind-address=0.0.0.0
--- p1_inception/srcs/requirements/wordpress/wp-script.sh
+++ p1_inception/srcs/requirements/wordpress/wp-script.sh
@@ -2,7 +2,7 @@
 
 # tail -f /dev/null
 if ! [ -f "/var/www/html/wp-config.php" ]; then
-	mv /root/html /var/www/
+	mv /root/html/* /var/www/html/
     wp config create --path='/var/www/html' --dbname='db_name' --dbuser='user_name' --dbpass='user_pass' --dbhost='mysql' --dbprefix='wp_' --allow-root
 	wp core install  --path='/var/www/html' --url='http://ereali.42.fr' --title='Blog Title' --admin_user='adminuser' --admin_password='password' --admin_email='ereali@student.42.fr' --allow-root
 	touch /var/log/access.log

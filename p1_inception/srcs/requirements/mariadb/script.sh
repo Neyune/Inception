a4f2ab5f6c3f338e99ac0b10c93e63b497e28776
lara <ereali@student.42.fr>
2023-01-28 05:32:41 +0100

    strange

--- p1_inception/srcs/requirements/mariadb/script.sh
+++ p1_inception/srcs/requirements/mariadb/script.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
-touch /tmp_file
-chmod 755 /tmp_file
+touch /tmp/tmp_file
+chmod 755 /tmp/tmp_file
 
 service mysql start
 
-cat << EOF > /tmp_file
-CREATE DATABASE db_name DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-GRANT ALL ON *.* TO 'user_name'@'localhost' IDENTIFIED BY 'user_pass';
+cat << EOF > /tmp/tmp_file
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
+FLUSH PRIVILEGES;
+GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
-mysql < /tmp_file
+mysql < /tmp/tmp_file
 # creer un fichier temporaire dans lequel on met les instruction mysql 
 # les rediriger dans mysql
 service mysql stop
+rm -rf /tmp/tmp_file
 
 exec mysqld --user=mysql --bind-address=0.0.0.0
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+
+
+# cat << EOF > /tmp/tmp_file
+# CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+# GRANT ALL ON *.* TO '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
+# FLUSH PRIVILEGES;
+# EOF
+
+
+
+# environment:
+  # WORDPRESS_DB_HOST=${WORDPRESS_DB_HOST}
+  # MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
+  # WORDPRESS_DB_NAME=${WORDPRESS_DB_NAME}
+  # WORDPRESS_DB_USER=${WORDPRESS_DB_USER}
+  # WORDPRESS_DB_PASSWORD=${WORDPRESS_DB_PASSWORD}
+  # WORDPRESS_TABLE_PREFIX=${WORDPRESS_TABLE_PREFIX}
+# environment:
+#   MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
+#   MYSQL_DATABASE=${MYSQL_DATABASE}
+#   MYSQL_USER=${MYSQL_USER}
+#   MYSQL_PASSWORD=${MYSQL_PASSWORD}
--- p1_inception/srcs/requirements/wordpress/wp-script.sh
+++ p1_inception/srcs/requirements/wordpress/wp-script.sh
@@ -2,12 +2,25 @@
 
 # tail -f /dev/null
 if ! [ -f "/var/www/html/wp-config.php" ]; then
-	mv /root/html /var/www/
-    wp config create --path='/var/www/html' --dbname='db_name' --dbuser='user_name' --dbpass='user_pass' --dbhost='mysql' --dbprefix='wp_' --allow-root
-	wp core install  --path='/var/www/html' --url='http://ereali.42.fr' --title='Blog Title' --admin_user='adminuser' --admin_password='password' --admin_email='ereali@student.42.fr' --allow-root
+	mv /root/html/* /var/www/html/
+    wp config create --path='/var/www/html' --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASSWORD} --dbhost=${WORDPRESS_DB_HOST} --dbprefix='wp_' --allow-root
+	wp core install  --path='/var/www/html' --url='https://ereali.42.fr' --title='Blog Title' --admin_user=${MYSQL_USER} --admin_password=${MYSQL_ROOT_PASSWORD} --admin_email='ereali@student.42.fr' --allow-root
 	touch /var/log/access.log
 	touch /var/log/error.log
 fi
-# tail -f /dev/null
 
 exec php-fpm7.3 -FR
+
+
+# environment:
+  # WORDPRESS_DB_HOST=${WORDPRESS_DB_HOST}
+  # MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
+  # WORDPRESS_DB_NAME=${WORDPRESS_DB_NAME}
+  # WORDPRESS_DB_USER=${WORDPRESS_DB_USER}
+  # WORDPRESS_DB_PASSWORD=${WORDPRESS_DB_PASSWORD}
+  # WORDPRESS_TABLE_PREFIX=${WORDPRESS_TABLE_PREFIX}
+# environment:
+#   MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
+#   MYSQL_DATABASE=${MYSQL_DATABASE}
+#   MYSQL_USER=${MYSQL_USER}
+#   MYSQL_PASSWORD=${MYSQL_PASSWORD}

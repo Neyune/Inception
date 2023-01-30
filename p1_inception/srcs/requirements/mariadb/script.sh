b9c5c717527b0e71be648bbb4115abe20f802510
Lara REALI <ereali@e1r11p6.clusters.42paris.fr>
2023-01-30 18:37:56 +0100

    paufinage preparation .env ajout wordpress user

--- p1_inception/srcs/requirements/mariadb/script.sh
+++ p1_inception/srcs/requirements/mariadb/script.sh
@@ -9,31 +9,20 @@ CREATE DATABASE IF NOT EXISTS db_name DEFAULT CHARACTER SET utf8 COLLATE utf8_un
 GRANT ALL ON db_name.* TO 'user_name'@'%' IDENTIFIED BY 'user_pass';
 FLUSH PRIVILEGES;
 EOF
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 mysql < /tmp/tmp_file
-# creer un fichier temporaire dans lequel on met les instruction mysql 
+# creer un fichier temporaire dans lequel on met les instruction mysql
 # les rediriger dans mysql
-# cat << EOF > /tmp_file
-# CREATE DATABASE IF NOT EXISTS db_name DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-# GRANT ALL ON *.* TO 'user_name'@'localhost' IDENTIFIED BY 'user_pass';
-# FLUSH PRIVILEGES;
-# EOF
-# mysql < /tmp/tmp_file
-# # # creer un fichier temporaire dans lequel on met les instruction mysql 
-# # # les rediriger dans mysql
 service mysql stop
 rm -rf /tmp/tmp_file
 
 exec mysqld --user=mysql --bind-address=0.0.0.0
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 
-
-# cat << EOF > /tmp/tmp_file
-# CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-# GRANT ALL ON *.* TO '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
+# CREATE DATABASE IF NOT EXISTS ${DB_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+# GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY ${DB_PWD};
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PWD}';
 # FLUSH PRIVILEGES;
 # EOF
-
-
 
 # environment:
   # WORDPRESS_DB_HOST=${WORDPRESS_DB_HOST}
--- p1_inception/srcs/requirements/wordpress/wp-script.sh
+++ p1_inception/srcs/requirements/wordpress/wp-script.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# tail -f /dev/null
 if ! [ -f "/var/www/html/wp-config.php" ]; then
 	mv /root/html/* /var/www/html/
     wp config create --path='/var/www/html' --dbname='db_name' --dbuser='user_name' --dbpass='user_pass' --dbhost='mysql' --dbprefix='wp_' --allow-root
+	# wp config create --path='/var/www/html' --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PWD} --dbhost='mysql' --dbprefix='wp_' --allow-root
 	touch /var/log/access.log
 	touch /var/log/error.log
 fi
-# tail -f /dev/null
+# wp user create bloom bloom@winx.com --role=author --user_pass=${WP_USER_PWD} --allow-root
 wp core install  --path='/var/www/html' --url='http://ereali.42.fr' --title='Blog Title' --admin_user='adminuser' --admin_password='password' --admin_email='ereali@student.42.fr' --allow-root
-
+# wp core install  --path='/var/www/html' --url='http://ereali.42.fr' --title='Inception' --admin_user=${WP_ADMIN} --admin_password=${WP_PWD} --admin_email='ereali@student.42.fr' --allow-root
 exec php-fpm7.3 -FR
 
 
